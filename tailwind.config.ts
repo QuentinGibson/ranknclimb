3529a00712802a97828b8f72c3f6d2cc4ec2ead4
Quentin Gibson <quentingibson94@gmail.com>
2023-12-27 17:18:02 -0500

    Add Storybook configuration and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './stories/**/*.{js,ts,jsx,tsx}'
   ],
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {
       gridTemplateColumns: {
